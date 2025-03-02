2edbc1b69f7befc7723d45f71e88a68b9ddde910
Milan Stojanovic <milances.14@gmail.com>
2025-03-02 23:14:35 +0100

    add cors to be allowed for all

--- GameRPSSL/settings.py
+++ GameRPSSL/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "gameapi.apps.GameapiConfig",
     "drf_spectacular",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -52,9 +53,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
-
-
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = "GameRPSSL.urls"
 
 TEMPLATES = [
